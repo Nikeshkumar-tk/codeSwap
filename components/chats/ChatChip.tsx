9c590469e75ac2ce7f0e663a9e7d6991fcf63cb9
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-05-27 20:32:38 +0530

    Chat modified

--- components/chats/ChatChip.tsx
+++ components/chats/ChatChip.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 type ChatChipType = {
     message:string,
     email:string
+    owner:boolean
 }
 const ChatChip = (props:ChatChipType) => {
-    const {message, email} = props
+    const {message, email, owner} = props
   return (
-    <div className='bg-blue-700 rounded-full text-white px-3 py-2'>{message}</div>
+    <div className={`${owner ? "bg-blue-700 text-white" : "bg-white text-black border-gray-400 border"} rounded-full px-3 py-2`}>{message}</div>
   )
 }
 
--- pages/chatRooms/[chatId].tsx
+++ pages/chatRooms/[chatId].tsx
@@ -7,6 +7,8 @@ import type { RequestBody as MessageType } from '../api/chats/sendMessage'
 import { BiSend } from "react-icons/bi"
 import ChatChip from '@/components/chats/ChatChip'
 import { useSession } from 'next-auth/react'
+import ScrollToBottom from 'react-scroll-to-bottom';
+
 const ChatPage = () => {
     const [chats, setChats] = useState<MessageType[] | null>([])
     const messageRef = useRef<any>()
@@ -48,21 +50,22 @@ const ChatPage = () => {
             <div className='h-[35rem] sm:h-[38rem] w-[45rem] rounded-lg shadow-lg px-5 py-3 flex flex-col'>
                 <h1 className='font-bold text-2xl'>Chat</h1>
                 <div className="w-full h-[1.5px] rounded-full bg-gray-600"></div>
-                <div className='relative flex-1 flex flex-col mt-1 overflow-scroll'>
+                <ScrollToBottom className='relative flex-1 flex flex-col mt-1 overflow-scroll'>
                     <div className='flex-1 flex flex-col w-full gap-1 overflow-y-scroll'>
                         {
                             chats?.map((chat: MessageType) => (
-                                <div className={`${userData?.user?.email === chat.email ? "self-end" : "self-start"}`} key={chat.message}>
-                                    <ChatChip email={chat.email} message={chat.message} />
+                                <div className={`${userData?.user?.email === chat.email ? "self-end" : "self-start"} relative`} key={chat.message}>
+                                    {userData?.user?.email !== chat.email && chat.email}
+                                    <ChatChip owner={userData?.user?.email === chat.email} email={chat.email} message={chat.message} />
                                 </div>
                             ))
                         }
                     </div>
                     <div className='flex items-center justify-between w-full'>
-                        <input type="text" ref={messageRef} placeholder='Enter something...' className='border-none outline-none border-b-2 w-full' />
+                        <input type="text" ref={messageRef} placeholder='Enter something...' className='border-none outline-none border-b-2 w-full' onKeyUp={(e) => e.key === "Enter" && handleSendMessage()}/>
                         <BiSend onClick={handleSendMessage} className='cursor-pointer'/>
                     </div>
-                </div>
+                </ScrollToBottom>
             </div>
 
         </div>
